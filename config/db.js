c7f1bb6b1824170c1fbb085513715dde10ccf148
nahuelvera00 <nahuelvera3770@gmail.com>
2022-05-11 17:41:04 -0300

    db-config

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 console.log(process.env.BD_NOMBRE)
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, '', {
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'mysql',
